3136b0e4bb47259fc68cd3e3b0d26f80dfa1ae0b
Georgi Manov <georgimanov@gmail.com>
2020-06-21 23:08:40 +0300

    Added PeripheralDevicesController.cs

---
+++ src/Web/Gateways.Web.ViewModels/Devices/PeripheralDeviceInputModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Gateways.Web.ViewModels.Devices
+{
+    using System;
+
+    using Gateways.Data.Models;
+    using Gateways.Services.Mapping;
+
+    public class PeripheralDeviceInputModel : IMapTo<PeripheralDevice>
+    {
+        public int UID { get; set; }
+
+        public string Vendor { get; set; }
+
+        public DateTime DateOfCreation { get; set; }
+
+        public DeviseStatus Status { get; set; }
+    }
+}
---
+++ src/Web/Gateways.Web/Controllers/PeripheralDevicesController.cs
@@ -0,0 +1,47 @@
+﻿namespace Gateways.Web.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Gateways.Data.Models;
+    using Gateways.Services.Data;
+    using Gateways.Services.Mapping;
+    using Gateways.Web.ViewModels.Devices;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PeripheralDevicesController : BaseApiController
+    {
+        private readonly IPeripheralDevicesService peripheralDevicesService;
+
+        public PeripheralDevicesController(IPeripheralDevicesService peripheralDevicesService)
+        {
+            this.peripheralDevicesService = peripheralDevicesService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(PeripheralDeviceInputModel model)
+        {
+            var device = AutoMapperConfig.MapperInstance.Map<PeripheralDeviceInputModel, PeripheralDevice>(model);
+
+            await this.peripheralDevicesService.Create(device);
+
+            var viewModel = AutoMapperConfig.MapperInstance.Map<PeripheralDevice, PeripheralDeviceViewModel>(device);
+
+            return this.CreatedAtAction(nameof(this.Create), new { id = viewModel.Id }, viewModel);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var device = await this.peripheralDevicesService.GetAsync(id);
+            if (device == null)
+            {
+                return this.NotFound();
+            }
+
+            await this.peripheralDevicesService.Delete(device);
+
+            return this.NoContent();
+        }
+    }
+}
